89b4e8eacd974a162c013da0967eda0acbaf3958
rzalegal <vsimanov@opinionsn.com>
2020-02-25 16:12:44 +0300

    TODO 25/02

--- src/main/java/parameter/BoundedParameter.java
+++ src/main/java/parameter/BoundedParameter.java
@@ -11,6 +11,7 @@ public class BoundedParameter extends Parameter {
         super(value);
         this.lowerBound = new GreaterThanParameter(value, lowerBound);
         this.upperBound = new LessThanParameter(value, upperBound);
-        
     }
+
+    // TODO: 25/02/2020 Создать в пакете parameter подпакет specific и определить в нем класс Intellect как наслледника BoundedParameter
 }
